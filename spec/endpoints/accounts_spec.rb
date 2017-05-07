0869d82f4844c1c496af6e13c43acfe0c368b9fd
Andrew Campbell <andycampbell92@gmail.com>
2017-05-07 19:28:45 +0100

    Added tests for getting single account and refactored spec

--- spec/endpoints/accounts_spec.rb
+++ spec/endpoints/accounts_spec.rb
@@ -3,7 +3,7 @@ require 'rspec'
 require 'rack/test'
 require 'oj'
 
-describe 'The accounts endpoint' do
+describe "/accounts endpoint" do
   include Rack::Test::Methods
 
   def app
@@ -18,36 +18,61 @@ describe 'The accounts endpoint' do
       {'address' => '0x8eeec35015baba2890e714e052dfbe73f4b752f9', 'balance' => 700000000000000000},
       {'address' => '0xfb663039763f61506f66158720f72794eddb1cc0', 'balance' => 800000000000000000}
     ]
+
     Account.multi_insert(@seeded_accounts)
   end
 
-  it "responds with ok to /accounts" do
-    get "/accounts"
-    expect(last_response.ok?).to be true
+  describe "get /accounts" do
+    it "responds with ok" do
+      get "/accounts"
+      expect(last_response.ok?).to be true
+    end
+
+    it "responds with a list of all accounts in the database" do
+      get "/accounts"
+      expect(Oj.load(last_response.body)).to eq @seeded_accounts
+    end
   end
 
-  it "responds with a list of all accounts in the database" do
-    get "/accounts"
-    expect(Oj.load(last_response.body)).to eq @seeded_accounts
+  describe "get /accounts/:address" do
+    it "responds with ok when given valid address" do
+      to_get = @seeded_accounts[0]
+      get "/accounts/#{to_get['address']}"
+      expect(last_response.ok?).to be true
+    end
+
+    it "responds with corresponding account when provided with address" do
+      to_get = @seeded_accounts[1]
+      get "/accounts/#{to_get['address']}"
+      expect(last_response.body).to eq to_get
+    end
+
+    it "responds with not_found when non existent address provided" do
+      get "/accounts/0x111111111111111111115abb99faa1867706ea9c"
+      expect(last_response.not_found?).to be true
+    end
+
+    it "responds with sensible message when non existent address provided" do
+      get "/accounts/0x111111111111111111115abb99faa1867706ea9c"
+      expect(Oj.load(last_response.body['message'])).to eq "an account with that address was not found in cache"
+    end
+
+    it "responds with ok to /accounts/:address get with valid address without prefix" do
+      to_get = @seeded_accounts[2]
+      get "/accounts/#{to_get['address'][2..-1]}"
+      expect(last_response.body).to eq to_get
+    end
   end
 
-  it "responds with ok when /accounts/:address posted with valid address" do
-    post "/accounts/0x2b9c4e2ad6f1e7bd43365abb99faa1867706ea9c"
-    expect(last_response.ok?).to be true
-  end
+  describe "post /accounts/:address" do
+    it "responds with ok when /accounts/:address posted with valid address" do
+      post "/accounts/0x2b9c4e2ad6f1e7bd43365abb99faa1867706ea9c"
+      expect(last_response.ok?).to be true
+    end
 
-  it "responds with ok when /accounts/:address posted with valid address without prefix" do
-    post "/accounts/2b9c4e2ad6f1e7bd43365abb99faa1867706ea9c"
-    expect(last_response.ok?).to be true
-  end
-
-  it "responds with ok to /accounts/:address get with valid address" do
-    get "/accounts/0x2b9c4e2ad6f1e7bd43365abb99faa1867706ea9c"
-    expect(last_response.ok?).to be true
-  end
-
-  it "responds with ok to /accounts/:address get with valid address without prefix" do
-    get "/accounts/2b9c4e2ad6f1e7bd43365abb99faa1867706ea9c"
-    expect(last_response.ok?).to be true
+    it "responds with ok when /accounts/:address posted with valid address without prefix" do
+      post "/accounts/2b9c4e2ad6f1e7bd43365abb99faa1867706ea9c"
+      expect(last_response.ok?).to be true
+    end
   end
 end
