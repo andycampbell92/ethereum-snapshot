4cfea3e1e0ef84269f9669605b5e0dff93b044b0
Andrew Campbell <andycampbell92@gmail.com>
2017-05-07 20:03:38 +0100

    validate_address method working

--- models/account.rb
+++ models/account.rb
@@ -1,4 +1,11 @@
 class Account < Sequel::Model
   def self.validate_address(address)
+    if address.length.between?(40, 42)
+      non_prefixed = address.length == 42 ? address[2..-1] : address
+      # if non_prefixed is hex
+      if !non_prefixed[/\H/]
+        return "0x" + non_prefixed
+      end
+    end
   end
 end
