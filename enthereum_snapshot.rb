a2aa925aab90bfd66bb93625ed8cb6676474e4ae
Andrew Campbell <andycampbell92@gmail.com>
2017-05-07 18:45:14 +0100

    get accounts endpoint working

--- enthereum_snapshot.rb
+++ enthereum_snapshot.rb
@@ -1,17 +1,23 @@
+require_relative "models"
 require "roda"
+require "oj"
+
 
 class EthereumSnapshot < Roda
+  plugin :json, :classes=>[Array, Hash], :serializer=>proc{|o| Oj.dump(o, :mode => :strict)}
+  
   route do |r|
     r.on "accounts" do
+      whitelist = [:address, :balance]
       r.is do
         r.get do
-          "hello from accounts"
+          Account.select(*whitelist).map {|o| o.values}
         end
       end
 
       r.is ":address" do |address|
         r.post do
-          "#{address}"
+          "post accounts"
         end
 
         r.get do
--- migrate/001_accounts_table_create.rb
+++ migrate/001_accounts_table_create.rb
@@ -2,7 +2,7 @@ Sequel.migration do
   up do
     create_table(:accounts) do
       primary_key :id
-      String :address, :null=>false
+      String :address, null: false, unique: true
       Integer :balance
     end
   end
